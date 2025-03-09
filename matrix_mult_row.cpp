857db120045c71205e1d9ba459ea7ffbc9a3a181
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2025-03-09 18:40:19 -0300

    matriz inicializada

---
+++ matrix_mult_row.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <random>
+
+
+int main(int argc, char *argv[]) {
+
+    const int N = argv[1];
+    std::vector<std::vector<int>> matriz(N, std::vector<int>(N, 1));
+
+
+    return 0;
+}
