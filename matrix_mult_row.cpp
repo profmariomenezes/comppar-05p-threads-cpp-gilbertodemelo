2b551b5d557d67c5388a8fb4814b69551f8db32b
Gilberto De Melo, Jr <gilberto.demelo@yahoo.com>
2025-03-09 18:47:40 -0300

    multiplicacao das matrizes feita

--- matrix_mult_row.cpp
+++ matrix_mult_row.cpp
@@ -7,7 +7,34 @@
 int main(int argc, char *argv[]) {
 
     const int N = argv[1];
-    std::vector<std::vector<int>> matriz(N, std::vector<int>(N, 1));
+    std::vector<std::vector<int>> matriz1(N, std::vector<int>(N));
+    std::vector<std::vector<int>> matriz2(N, std::vector<int>(N));
+    std::vector<std::vector<int>> resultado(N, std::vector<int>(N));
+
+    // Gerar números aleatórios que preencherão a matriz
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distrib(1, 100); // entre 1 e 100
+
+    // preencher a matriz1
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            matriz1[i][j] = distrib(gen);
+        }
+    }
+
+    // preencher matriz2
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            matriz2[i][j] = distrib(gen);
+        }
+    }
+
+    // Multiplicação de matriz por matriz por linha
+    auto inicio = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            resultado[i][j] = matriz1[i][j] * matriz2[i][j];
 
 
     return 0;
